03d2b38fb75e6d08618161d583e70ce0abdd562b
Lukasz Pasko <flucky500@gmail.com>
2022-03-09 23:38:51 +0100

    Nie jestem pewien czy o to chodzi.

--- src/main/java/pl/pasko/ConsoleMessage.java
+++ src/main/java/pl/pasko/ConsoleMessage.java
@@ -3,4 +3,13 @@ package pl.pasko;
 public class ConsoleMessage extends Message{
 
     private String consoleMessage;
+
+    public ConsoleMessage() {
+    }
+
+    @Override
+    String getMessage() {
+        this.consoleMessage = "This class is a ConsoleMessage";
+        return consoleMessage;
+    }
 }
--- src/main/java/pl/pasko/Main.java
+++ src/main/java/pl/pasko/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+    ConsoleMessage consoleMessage = new ConsoleMessage();
+        System.out.println(consoleMessage.getMessage());
+
+    WindowMessage windowMessage = new WindowMessage();
+        System.out.println(windowMessage.getMessage());
+
+
+
+
     }
 }
